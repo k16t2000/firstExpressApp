42bab261ad5b1bf5422638e77316ca28b6bd7d99
Opilane TTHK <opilane@tthk.ee>
2020-09-09 11:32:57 +0300

    First Express App

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+
+app.get("/", function(request, response){
+    //console.log(request);
+    response.send('<h1>Hello World!</h1>');
+});
+
+app.get("/about", function(req,res){
+    res.send('<h3>This is About!</h3>');
+});
+
+app.get("/kontakt", function(requ,resu){
+    resu.send('<h3>kt@mail.ru</h3>');
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on Port 3000.");
+});
